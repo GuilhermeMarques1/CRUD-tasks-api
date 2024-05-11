448849d7c3e4be617bd4f94afe7e2a81962d536c
GuilhermeMarques1 <g-marques@outlook.com>
2024-05-11 18:15:24 -0300

    dev: Handle route and query parameters

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import http from "node:http";
 import { formatRequest } from "./middlewares/format-request.js";
 import { routes } from "./routes.js";
+import { extractQueryParameters } from "./utils/extract-query-parameters.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   await formatRequest(req, res);
   
   const route = routes.find(route => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if(route) {
+    const routeParams = req.url.match(route.path);
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParameters(query) : {};
+
     return route.handler(req, res);
   }
 
