8ab577a3174c29739dedb415475928626820873e
GuilhermeMarques1 <g-marques@outlook.com>
2024-05-11 16:32:12 -0300

    dev: Config routes and request body

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 import http from "node:http";
+import { formatRequest } from "./middlewares/format-request.js";
+import { routes } from "./routes.js";
 
-const server = http.createServer((request, response) => {
-  return response.end("Hello, world");
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+  await formatRequest(req, res);
+  
+  const route = routes.find(route => {
+    return route.method === method && route.path === url;
+  });
+
+  if(route) {
+    return route.handler(req, res);
+  }
+
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
