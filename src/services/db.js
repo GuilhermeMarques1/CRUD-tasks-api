4c8d0db067a53022000406cc1e430f563ef0dd47
GuilhermeMarques1 <g-marques@outlook.com>
2024-05-11 16:47:51 -0300

    dev: Config database constructor

--- src/services/db.js
+++ src/services/db.js
@@ -1,9 +1,17 @@
 import fs from "node:fs/promises";
 
-const dbPath = new URL("../database/db.json", import.meta.url);
+const dbPath = new URL("../../database/db.json", import.meta.url);
 
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(dbPath)
+      .then((data) => (this.#database = JSON.parse(data)))
+      .catch(() => {
+        this.#persist()
+      });
+  }
 
   #persist() {
     fs.writeFile(dbPath, JSON.stringify(this.#database));
