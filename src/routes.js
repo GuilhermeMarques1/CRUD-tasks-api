726eb6c515be3b436344eeb569b427ea1dc4ca16
GuilhermeMarques1 <g-marques@outlook.com>
2024-05-11 16:48:19 -0300

    feat: Add create task route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,29 @@
+import { Database } from "./services/db.js";
+import { randomUUID } from "node:crypto";
+
+const database = new Database();
+
 export const routes = [
   {
     method: "POST",
-    path: "/task",
+    path: "/tasks",
     handler: (req, res) => {
+      const { title, description } = req.body;
       
+      if(!title || !description) {
+        return res.writeHead(400).end("It's missing data");
+      } 
+
+      database.insert("tasks", {
+        id: randomUUID(),
+        title,
+        description,
+        created_at: new Date().toString(),
+        completed_at: null,
+        updated_at: null,
+      });
+
+      return res.writeHead(201).end();
     }
   }
 ]
